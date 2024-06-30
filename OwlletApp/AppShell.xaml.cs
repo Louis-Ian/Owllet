bb79ca4b3d7fc64616834cd5d6f7ae014c46d64b
Louis dos Santos <louisiangt@gmail.com>
2024-06-30 09:47:51 -0300

    starting

---
+++ OwlletApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace OwlletApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ OwlletApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace OwlletApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
